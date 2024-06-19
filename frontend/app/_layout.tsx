b77de8a0a9cbade035a6f16e7bbf80ba0bdfbce9
Kelvin229 <ke20od@gmail.com>
2024-06-19 11:14:03 -0400

    first doc

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,10 +4,10 @@ import React from "react";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="Login" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
       <Stack.Screen name="SignUp" options={{ headerShown: false }} />
       <Stack.Screen name="ForgotPassword" options={{ headerShown: false }} />
-      <Stack.Screen name="Login" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
